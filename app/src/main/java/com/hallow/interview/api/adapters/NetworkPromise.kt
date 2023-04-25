41e5e3b8b564aee6f5910fe78bb6d00c18811979
John Paul McKean <johnpaulmckean@users.noreply.github.com>
2023-04-25 12:30:32 -0500

    Updated

--- app/src/main/java/com/hallow/interview/api/adapters/NetworkPromise.kt
+++ app/src/main/java/com/hallow/interview/api/adapters/NetworkPromise.kt
@@ -19,15 +19,15 @@ class NetworkPromise<R>(private val call: Call<R>) {
 
         val callback = object : Callback<R> {
 
-            override fun onFailure(call: Call<R>?, t: Throwable?) {
+            override fun onFailure(call: Call<R>, t: Throwable) {
                 when (t) {
                     is Exception -> deferred.reject(t)
-                    else -> deferred.reject(Exception(t?.localizedMessage))
+                    else -> deferred.reject(Exception(t.localizedMessage))
                 }
             }
 
-            override fun onResponse(call: Call<R>?, response: Response<R>?) {
-                if (response?.isSuccessful == false && response.code() in ERROR_CODE_REANGE) {
+            override fun onResponse(call: Call<R>, response: Response<R>) {
+                if (!response.isSuccessful && response.code() in ERROR_CODE_RANGE) {
                     val errorJsonString = response.errorBody()?.string()
                     val message = try {
                         JsonParser().parse(errorJsonString).asJsonObject["error"].asString
@@ -37,7 +37,7 @@ class NetworkPromise<R>(private val call: Call<R>) {
 
                     deferred.reject(ApiException(message))
                 } else {
-                    response?.body()?.let {
+                    response.body()?.let {
                         deferred.resolve(it)
                     } ?: deferred.reject(Exception("Unable to parse response body."))
                 }
@@ -50,6 +50,6 @@ class NetworkPromise<R>(private val call: Call<R>) {
     }
 
     companion object {
-        val ERROR_CODE_REANGE = 400..511
+        val ERROR_CODE_RANGE = 400..511
     }
 }
